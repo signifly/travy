d99c97a2c2614434ce43002bba2a23134b802b24
mtm-sig <mtm@signifly.com>
2018-02-13 12:35:01 +0100

    sequential batch: back button, exit button, loading

--- app/components/panel.vue
+++ app/components/panel.vue
@@ -1,8 +1,11 @@
 <template>
 	<transition name="el-zoom-in-bottom" appear>
 		<div class="panel">
-			<div class="wrap">
+			<div class="wrap loading">
 				<slot/>
+				<transition name="overlay">
+					<div class="overlay" v-if="loading" />
+				</transition>
 			</div>
 		</div>
 	</transition>
@@ -10,7 +13,9 @@
 
 <script>
 export default {
-
+	props: {
+		loading: {type: Boolean, required: false}
+	}
 };
 </script>
 
@@ -32,6 +37,23 @@ export default {
 		border-top: 1px solid $blue3;
 		height: em(60);
 		padding: 0 1em;
+	}
+
+	.overlay {
+		background-color: rgba($white1, .7);
+		position: absolute;
+		z-index: 1;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+
+		&-enter-active, &-leave-active {
+			transition: cubic(opacity, 0.2s);
+		}
+		&-enter, &-leave-to {
+			opacity: 0;
+		}
 	}
 }
 </style>
--- app/components/view/components/panels/batch.vue
+++ app/components/view/components/panels/batch.vue
@@ -1,63 +1,134 @@
 <template>
-	<vPanel>
-		<div class="text">{{title}}</div>
+	<vPanel v-bind="{loading: loadingAll}">
+		<div class="left">
+			<Tooltip content="Close and save">
+				<a class="close" @click="closeSave"><i class="el-icon-circle-close-outline"></i></a>
+			</Tooltip>
+			<div class="text">{{title}}</div>
+		</div>
+
 		<div class="progress">
 			<div class="line"><Progress :percentage="progress" :show-text="false" /></div>
 			<div class="text">{{index + 1}} / {{items.length}}</div>
 		</div>
+
 		<div class="actions">
-			<Button v-if="next" size="medium" type="success" v-bind="{loading}" @click="save">Save and next</Button>
-			<Button v-else size="medium" type="success" icon="el-icon-check" v-bind="{loading}" @click="save">Save and exit</Button>
+			<Button v-if="prev" size="medium" type="success" icon="el-icon-arrow-left" @click="prevSave">
+				Back and save
+			</Button>
+
+			<Button v-if="next" size="medium" type="success" @click="nextSave">
+				Next and save
+				<i class="el-icon-arrow-right el-icon-right"></i>
+			</Button>
+
+			<Button v-if="!next" size="medium" type="success" @click="closeSave">
+				Exit and save
+				<i class="el-icon-check el-icon-right"></i>
+			</Button>
+
 		</div>
 	</vPanel>
 </template>
 
 <script>
-import {Button, Progress} from "element-ui";
+import {Button, Tooltip, Progress} from "element-ui";
 import vPanel from "@/components/panel.vue";
 
 export default {
-	components: {Button, Progress, vPanel},
+	components: {Button, Tooltip, Progress, vPanel},
 	props: {
 		title: {type: String, required: true},
 		loading: {type: Boolean, required: true},
 		seq: {type: Object, required: true},
 		getData: {type: Function, required: true}
 	},
+	data() {
+		return {
+			loadingData: false
+		}
+	},
 	computed: {
+		progress: (t) => ((t.index + 1) / t.items.length) * 100,
+		items: (t) => t.seq.items.map(x => x.toString()),
 		id: (t) => t.$route.params.id.toString(),
 		index: (t) => t.items.indexOf(t.id),
-		items: (t) => t.seq.items.map(x => x.toString()),
+		prev: (t) => t.items[t.index - 1],
 		next: (t) => t.items[t.index + 1],
-		progress: (t) => ((t.index + 1) / t.items.length) * 100
+
+		loadingAll: (t) => t.loading || t.loadingData
 	},
 	methods: {
-		save() {
+		nextSave() {
 			this.$parent.$emit("save", {
 				done: async () => {
-					if (this.next) {
-						this.$router.push({
-							params: {...this.$route.params, id: this.next},
-							query: {seq: {items: this.items}}
-						});
-
-						await this.$nextTick(); // wait for id to change
-						await this.getData();
-
-					} else {
-						this.$router.push("/" + this.$route.meta.parent.id);
-					}
+					this.$router.push({
+						params: {...this.$route.params, id: this.next},
+						query: {seq: {items: this.items}}
+					});
 				}
 			});
+		},
+
+		prevSave() {
+			this.$parent.$emit("save", {
+				done: async () => {
+					this.$router.push({
+						params: {...this.$route.params, id: this.prev},
+						query: {seq: {items: this.items}}
+					});
+				}
+			});
+		},
+
+		closeSave() {
+			this.$parent.$emit("save", {
+				done: async () => {
+					this.$router.push("/" + this.$route.meta.parent.id);
+				}
+			});
+		},
+
+		async _getData() {
+			try {
+				this.loadingData = true;
+				await this.getData();
+			} catch (err) {} finally {
+				this.loadingData = false;
+			}
+		}
+	},
+	watch: {
+		id() {
+			this._getData();
 		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
-.text {
-	font-size: em(14);
-	color: $blue4;
+.left {
+	display: flex;
+	align-items: center;
+
+	.close {
+		color: $danger;
+		padding: 0.5em;
+		margin-top: 0.2em;
+		margin-left: -0.5em;
+		font-size: 1.1em;
+
+		&.loading {
+			color: $blue3;
+			pointer-events: none;
+		}
+	}
+
+	.text {
+		margin-left: 0.5em;
+		font-size: em(14);
+		color: $blue4;
+	}
 }
 
 .progress {
@@ -75,5 +146,10 @@ export default {
 		margin-left: 1em;
 		letter-spacing: -1.25px;
 	}
+}
+
+.actions {
+	display: flex;
+	align-items: center;
 }
 </style>
