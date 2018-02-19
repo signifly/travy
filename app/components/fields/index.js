3162f2a42640af01e4f0b23feea1cceddbd375eb
mtm-sig <mtm@signifly.com>
2018-02-19 14:39:43 +0100

    add vTextarea

--- app/components/fields/index.js
+++ app/components/fields/index.js
@@ -12,6 +12,7 @@ export {default as vActions} from "./actions/index.vue";
 export {default as vInput} from "./input.vue";
 export {default as vInputNumber} from "./input-number.vue";
 export {default as vInputPassword} from "./input-password.vue";
+export {default as vTextarea} from "./textarea.vue";
 export {default as vEditor} from "./editor.vue";
 export {default as vLine} from "./line.vue";
 export {default as vTable} from "./table.vue";
---
+++ app/components/fields/textarea.vue
@@ -0,0 +1,49 @@
+<template>
+	<div class="textarea">
+		<Input v-model="data.content" @input="update" v-bind="{disabled}" type="textarea" :rows="3" :controls="false" size="medium" />
+	</div>
+</template>
+
+<script>
+import {Input} from "element-ui";
+
+export default {
+	components: {Input},
+	meta: {
+		res: {
+			props: {
+				content: "text"
+			},
+			data: {
+				text: ""
+			}
+		}
+	},
+	props: {
+		content: {type: String, required: false, doc: true},
+		_content: {type: String, required: true},
+		disabled: {type: Boolean, required: false}
+	},
+	data() {
+		return {
+			data: {
+				value: this.content
+			}
+		}
+	},
+	methods: {
+		update(content) {
+			this.$emit("fieldA", {
+				action: "update",
+				data: {[this._content]: content}
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.textarea {
+
+}
+</style>
