316de29b7d00e5cdbcb18c520e31e662e828b51f
mtm-sig <mtm@signifly.com>
2018-02-22 14:46:08 +0100

    add vColorPicker

--- app/components/fields/index.js
+++ app/components/fields/index.js
@@ -4,6 +4,7 @@ export {default as vStatus} from "./status.vue";
 export {default as vImage} from "./image.vue";
 export {default as vSwitch} from "./switch.vue";
 export {default as vCheckbox} from "./checkbox.vue";
+export {default as vColorPicker} from "./colorPicker.vue";
 export {default as vSelect} from "./select.vue";
 export {default as vSelectSearch} from "./select-search.vue";
 export {default as vSelectMulti} from "./select-multi.vue";
