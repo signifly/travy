9c603412d86df8302a7d17507b4447b7b3f0f74a
mtm-sig <mtm@signifly.com>
2018-03-13 15:19:44 +0100

    redirect index to products

--- app/modules/router.js
+++ app/modules/router.js
@@ -32,7 +32,7 @@ const routesViews = map(tables, (item, id) => ({
 }));
 
 const routes = [
-	{path: "/", name: "index", component: index, meta: {auth: {}}},
+	{path: "/", name: "index", component: index, redirect: "/products", meta: {auth: {}}},
 	{path: "/login", name: "login", component: login, props: true, meta: {layout: "vBase"}},
 	{path: "/login/reset/:id", name: "login-reset", component: loginReset, props: true, meta: {layout: "vBase"}},
 	{path: "/doc", name: "doc", component: doc, meta: {layout: "vBase", auth: {}}},
