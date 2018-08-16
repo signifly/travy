35370f1f3ffac9f5fd0ada798e9ceda2eaaa141f
mtmsig <mtm@signifly.com>
2018-08-16 12:57:48 +0200

    show action download

--- src/components/actions/show.vue
+++ src/components/actions/show.vue
@@ -1,22 +1,33 @@
-<template>
-	<div class="show"></div>
-</template>
-
 <script>
 import {endpointUrl} from "@/modules/utils";
 
 export default {
 	props: {
 		alt: {type: Object, required: true},
+		data: {type: Object, required: false},
 		endpoint: {type: Object, required: true},
-		data: {type: Object, required: false}
+		download: {type: Boolean, required: false}
 	},
+	render: () => ({}),
 	computed: {
 		dataComb: (t) => ({...t.alt.data, ...t.data}),
 		endpointUrl: (t) => endpointUrl({data: t.dataComb, url: t.endpoint.url})
 	},
+	methods: {
+		downloadFile() {
+			const link = document.createElement("a");
+			link.href = this.endpointUrl;
+			link.download = true;
+			link.click();
+		},
+
+		go() {
+			this.$router.push({path: this.endpointUrl, query: {modifiers: this.$route.query.modifiers}});
+		}
+	},
 	created() {
-		this.$router.push({path: this.endpointUrl, query: {modifiers: this.$route.query.modifiers}});
+		this.$emit("close");
+		this.download ? this.downloadFile() : this.go();
 	}
 };
 </script>
