c1ddb6c56217515ce424e924ccef1c3de0eef7b7
mtmsig <mtm@signifly.com>
2019-01-04 16:14:55 +0100

    fix favicon, npm release

--- src/modules/favicon.js
+++ src/modules/favicon.js
@@ -1,17 +1,14 @@
-const favicon = document.querySelector("link[rel=icon]");
+const canvas = document.createElement("canvas");
+canvas.height = 64;
+canvas.width = 64;
 
-if (favicon) {
-	const emoji = favicon.getAttribute("data-emoji");
+const ctx = canvas.getContext("2d");
+ctx.font = "64px serif";
+ctx.fillText("🔹", 0, 64);
 
-	if (emoji) {
-		const canvas = document.createElement("canvas");
-		canvas.height = 64;
-		canvas.width = 64;
+const favicon = document.createElement("link");
+favicon.setAttribute("type", "image/png");
+favicon.setAttribute("rel", "icon");
+favicon.setAttribute("href", canvas.toDataURL());
 
-		const ctx = canvas.getContext("2d");
-		ctx.font = "64px serif";
-		ctx.fillText(emoji, 0, 64);
-
-		favicon.href = canvas.toDataURL();
-	}
-}
+document.querySelector("head").appendChild(favicon);
