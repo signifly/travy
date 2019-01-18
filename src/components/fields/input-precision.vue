a50ba3e5469c84243a7f41f2079e7c3c4da75eba
mtmsig <mtm@signifly.com>
2019-01-18 12:53:52 +0100

    add input-precision field

---
+++ src/components/fields/input-precision.vue
@@ -0,0 +1,78 @@
+<template>
+	<div class="input" @keypress="validate">
+		<InputNumber
+			v-model="valueC"
+			:disabled="_disabled"
+			:precision="_precision"
+			:step="_step"
+			:max="_max"
+			:controls="true"
+			size="medium"
+			@input="update"
+		/>
+	</div>
+</template>
+
+<script>
+import {toNumber, debounce} from "lodash";
+import {InputNumber} from "element-ui";
+
+export default {
+	components: {InputNumber},
+	meta: {
+		res: {
+			props: {
+				disabled: false,
+				value: "inputVal",
+				precision: 2,
+				step: 0.1,
+				max: 10
+			},
+			data: {
+				inputVal: 3.20
+			}
+		}
+	},
+	props: {
+		alt: {type: Object, required: true},
+		_disabled: {type: Boolean, required: false, doc: true},
+		_precision: {type: Number, required: true, doc: true},
+		_step: {type: Number, required: true, doc: true},
+		_max: {type: Number, required: true, doc: true},
+		value: {type: Number, required: false, doc: true},
+		_value: {type: String, required: true},
+	},
+	data() {
+		return {
+			valueC: this.value
+		}
+	},
+	computed: {
+		wait: (t) => t.alt.type === "table" ? 500 : 0
+	},
+	methods: {
+		validate(e) {
+			if (e.key === ".") return;
+			if (isNaN(toNumber(e.key))) e.preventDefault();
+		},
+		update(value) {
+			this.$emit("fieldA", {
+				action: "update",
+				data: {[this._value]: value}
+			});
+		}
+	},
+	created() {
+		this.update = debounce(this.update, this.wait);
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.input {
+	position: relative;
+	.el-input-number {
+		width: 100%;
+	}
+}
+</style>
