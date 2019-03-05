02fe8eb43cc7cfae2acda8f74e14db192b229f9b
mtmsig <mtm@signifly.com>
2019-03-05 13:25:32 +0100

    field: radio-group

---
+++ src/components/fields/radio-group.vue
@@ -0,0 +1,56 @@
+<template>
+	<div class="radio-group">
+		<RadioGroup :value="value" size="medium" @input="update">
+			<RadioButton
+			v-for="item in _items"
+			:label="item.value"
+			:disabled="item.disabled"
+			:key="item.value">
+				{{item.label}}
+			</RadioButton>
+		</RadioGroup>
+	</div>
+</template>
+
+<script>
+import {RadioGroup, RadioButton} from "element-ui";
+
+export default {
+	components: {RadioGroup, RadioButton},
+	meta: {
+		res: {
+			props: {
+				value: "option",
+				items: [
+					{label: "Option A", value: 1},
+					{label: "Option B", value: 2},
+					{label: "Option C", value: 3},
+					{label: "Option D", value: 4, disabled: true}
+				]
+			},
+			data: {
+				option: 1
+			}
+		}
+	},
+	props: {
+		value: {type: [String, Number], required: false, doc: true},
+		_items: {type: Array, required: true, doc: true},
+		_value: {type: String, required: true}
+	},
+	methods: {
+		update(value) {
+			this.$emit("fieldA", {
+				action: "update",
+				data: {[this._value]: value}
+			});
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.radio-group {
+
+}
+</style>
