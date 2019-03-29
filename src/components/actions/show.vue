8ef7d7cb29fd83c37610ad51364332f541974927
Morten Poul Jensen <mpj@signifly.com>
2019-03-29 11:06:15 +0100

    Add external prop to show action

--- src/components/actions/show.vue
+++ src/components/actions/show.vue
@@ -2,7 +2,8 @@
 export default {
 	props: {
 		endpoint: {type: Object, required: true},
-		download: {type: Boolean, required: false}
+		download: {type: Boolean, required: false},
+		external: {type: Boolean, required: false}
 	},
 	render: () => ({}),
 	methods: {
@@ -14,6 +15,11 @@ export default {
 		},
 
 		go() {
+			if (this.external) {
+				window.location = this.endpoint.url;
+				return;
+			}
+
 			this.$router.push({path: this.endpoint.url, query: {modifiers: this.$route.query.modifiers}});
 		}
 	},
