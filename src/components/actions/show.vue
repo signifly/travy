ed4b0026d77cceed16d646d40b7e1183ef4065d0
mtmsig <mtm@signifly.com>
2019-03-29 11:15:46 +0100

    Revert "Add external prop to show action"
    This reverts commit 8ef7d7cb29fd83c37610ad51364332f541974927.

--- src/components/actions/show.vue
+++ src/components/actions/show.vue
@@ -2,8 +2,7 @@
 export default {
 	props: {
 		endpoint: {type: Object, required: true},
-		download: {type: Boolean, required: false},
-		external: {type: Boolean, required: false}
+		download: {type: Boolean, required: false}
 	},
 	render: () => ({}),
 	methods: {
@@ -15,11 +14,6 @@ export default {
 		},
 
 		go() {
-			if (this.external) {
-				window.location = this.endpoint.url;
-				return;
-			}
-
 			this.$router.push({path: this.endpoint.url, query: {modifiers: this.$route.query.modifiers}});
 		}
 	},
