0aa95a8d7cd2f394d286139229f66b2489575caf
mtmsig <mtm@signifly.com>
2019-11-20 14:18:00 +0100

    table sort

--- src/components/tabs/tab/table/components/panel/move/index.vue
+++ src/components/tabs/tab/table/components/panel/move/index.vue
@@ -1,41 +1,7 @@
 <template>
 	<div class="move">
 		<Popover placement="top" width="300" ref="popover">
-			<div class="pop">
-				<div class="text">
-					Move to position out of 1334
-				</div>
-
-				<div class="position">
-					<Select
-						v-model="value"
-						size="small"
-						placeholder="Select"
-						:style="{width: '60%'}"
-					>
-						<Option label="Top" value="top" />
-						<Option label="Bottom" value="bottom" />
-						<Option label="Position" value="position" />
-					</Select>
-
-					<InputNumber
-						v-if="value === 'position'"
-						v-model="position"
-						controls-position="right"
-						:style="{width: '35%'}"
-						size="small"
-						:min="1"
-						:max="10"
-					/>
-				</div>
-
-				<div class="bottom">
-					<Button size="mini" type="primary" :disabled="!value" @click="move">
-						Confirm
-					</Button>
-				</div>
-			</div>
-
+			<popover @close="close" />
 			<Button size="medium" slot="reference">
 				Move
 				<i class="el-icon-d-caret el-icon-right" />
@@ -45,22 +11,17 @@
 </template>
 
 <script>
-import {Popover, Button, Select, Option, InputNumber} from "element-ui";
+import {Popover, Button} from "element-ui";
+import popover from "./popover";
 
 export default {
-	components: {Popover, Button, Select, Option, InputNumber},
+	components: {popover, Popover, Button},
 	props: {
 		sort: {type: Object, required: true}
 	},
-	data: () => ({
-		loading: false,
-		position: 0,
-		value: ""
-	}),
 	methods: {
-		move() {
+		close() {
 			this.$refs.popover.showPopper = false;
-			this.value = "";
 		}
 	}
 };
@@ -70,22 +31,5 @@ export default {
 .move {
 	display: flex;
 	align-items: center;
-
-	@at-root .pop {
-		padding: 0.75em;
-		font-size: 12px;
-		font-weight: 500;
-
-		.position {
-			margin: 1em 0 2em;
-			display: flex;
-			justify-content: space-between;
-		}
-
-		.bottom {
-			display: flex;
-			justify-content: flex-end;
-		}
-	}
 }
 </style>
--- src/components/tabs/tab/table/components/panel/move/popover.vue
+++ src/components/tabs/tab/table/components/panel/move/popover.vue
@@ -1,9 +1,74 @@
 <template>
-	<div class="popover"></div>
+	<div class="popover">
+		<div class="text">
+			Move to position out of 1334
+		</div>
+
+		<div class="position">
+			<Select
+				v-model="value"
+				size="small"
+				placeholder="Select"
+				:style="{width: '60%'}"
+			>
+				<Option label="Top" value="top" />
+				<Option label="Bottom" value="bottom" />
+				<Option label="Position" value="position" />
+			</Select>
+
+			<InputNumber
+				v-if="value === 'position'"
+				v-model="position"
+				controls-position="right"
+				:style="{width: '35%'}"
+				size="small"
+				:min="1"
+				:max="10"
+			/>
+		</div>
+
+		<div class="bottom">
+			<Button size="mini" type="primary" :disabled="!value" @click="move">
+				Confirm
+			</Button>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {};
+import {Button, Select, Option, InputNumber} from "element-ui";
+
+export default {
+	components: {Button, Select, Option, InputNumber},
+	data: () => ({
+		loading: false,
+		position: 0,
+		value: ""
+	}),
+	methods: {
+		move() {
+			this.value = "";
+			this.$emit("close");
+		}
+	}
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.popover {
+	padding: 0.75em;
+	font-size: 12px;
+	font-weight: 500;
+
+	.position {
+		margin: 1em 0 2em;
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.bottom {
+		display: flex;
+		justify-content: flex-end;
+	}
+}
+</style>
