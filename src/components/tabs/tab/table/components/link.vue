94c7d61a0018be0e800e7c2c04d6cc5403274e5a
mtmsig <mtm@signifly.com>
2019-12-05 12:00:20 +0100

    table link

---
+++ src/components/tabs/tab/table/components/link.vue
@@ -0,0 +1,42 @@
+<template>
+	<router-link :to="link" class="link">
+		<div class="back" />
+		<i class="el-icon-info" />
+	</router-link>
+</template>
+
+<script>
+export default {
+	props: {
+		link: {type: String, required: true}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.link {
+	position: absolute;
+	bottom: 0;
+	right: 0;
+
+	i {
+		position: relative;
+		font-size: 0.85em;
+		color: $blue4;
+		$s: 1px;
+		margin: 0 $s $s 0;
+	}
+
+	.back {
+		$s: 28px;
+
+		border-left: $s solid transparent;
+		border-bottom: $s solid $blue2;
+		position: absolute;
+		height: 0;
+		bottom: 0;
+		width: 0;
+		right: 0;
+	}
+}
+</style>
