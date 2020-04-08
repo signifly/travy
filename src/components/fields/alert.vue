278cd59f294b5db833f959de5df1e396550e31ee
mtmsig <mtm@signifly.com>
2020-04-08 11:54:02 +0200

    alert field

---
+++ src/components/fields/alert.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="alert">
+		<Alert
+			v-bind="{title, description}"
+			:closable="false"
+			:showIcon="icon"
+			:type="status"
+		/>
+	</div>
+</template>
+
+<script>
+import {Alert} from "element-ui";
+
+export default {
+	components: {Alert},
+	meta: {
+		spec: "props",
+		res: {
+			props: {
+				title: "Alert"
+			}
+		}
+	},
+	props: {
+		description: {type: String, required: false},
+		status: {type: String, default: "info"},
+		icon: {type: Boolean, required: false},
+		title: {type: String, required: true}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.alert {
+}
+</style>
